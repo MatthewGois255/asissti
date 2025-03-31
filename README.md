3582b8e503cea72e55ab33f769266bc9df154167
matheus <matheusgois.pessoal@gmail.com>
2025-03-31 17:17:37 -0300

    o do profe

--- README.md
+++
@@ -1,6 +0,0 @@
-![GitHub License](https://img.shields.io/github/license/MatthewGois255/asissti)
-
-# ASSISTI
-Desenvolvimento de um sistema desktop para gestão de ordens de serviços de uma assistência técnica de computadores e notebooks
-## Autor
-Matheus Gois
---
+++ main.js
@@ -0,0 +1,333 @@
+console.log("Processo principal")
+
+const { app, BrowserWindow, nativeTheme, Menu, ipcMain, dialog, shell } = require('electron')
+
+// Esta linha está relacionada ao preload.js
+const path = require('node:path')
+
+// Importação dos métodos conectar e desconectar (módulo de conexão)
+const { conectar, desconectar } = require('./database.js')
+
+// Importação do Schema Clientes da camada model
+const clientModel = require('./src/models/Clientes.js')
+
+// Importação do pacote jspdf (npm i jspdf)
+const { jspdf, default: jsPDF } = require('jspdf')
+
+// Importação da biblioteca fs (nativa do JavaScript) para manipulação de arquivos (no caso arquivos pdf)
+const fs = require('fs')
+
+// Janela principal
+let win
+const createWindow = () => {
+    // a linha abaixo define o tema (claro ou escuro)
+    nativeTheme.themeSource = 'light' //(dark ou light)
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        //autoHideMenuBar: true,
+        //minimizable: false,
+        resizable: false,
+        //ativação do preload.js
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    // menu personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
+    win.loadFile('./src/views/index.html')
+}
+
+// Janela sobre
+function aboutWindow() {
+    nativeTheme.themeSource = 'light'
+    // a linha abaixo obtém a janela principal
+    const main = BrowserWindow.getFocusedWindow()
+    let about
+    // Estabelecer uma relação hierárquica entre janelas
+    if (main) {
+        // Criar a janela sobre
+        about = new BrowserWindow({
+            width: 360,
+            height: 220,
+            autoHideMenuBar: true,
+            resizable: false,
+            minimizable: false,
+            parent: main,
+            modal: true
+        })
+    }
+    //carregar o documento html na janela
+    about.loadFile('./src/views/sobre.html')
+}
+
+// Janela cliente
+let client
+function clientWindow() {
+    nativeTheme.themeSource = 'light'
+    const main = BrowserWindow.getFocusedWindow()
+    if (main) {
+        client = new BrowserWindow({
+            width: 1010,
+            height: 680,
+            //autoHideMenuBar: true,
+            //resizable: false,
+            parent: main,
+            modal: true,
+            //ativação do preload.js
+            webPreferences: {
+                preload: path.join(__dirname, 'preload.js')
+            }
+        })
+    }
+    client.loadFile('./src/views/cliente.html')
+    client.center() //iniciar no centro da tela   
+}
+
+// Janela OS
+let os
+function osWindow() {
+    nativeTheme.themeSource = 'light'
+    const main = BrowserWindow.getFocusedWindow()
+    if (main) {
+        os = new BrowserWindow({
+            width: 1010,
+            height: 720,
+            // autoHideMenuBar: true,
+            resizable: false,
+            parent: main,
+            modal: true
+        })
+    }
+    os.loadFile('./src/views/os.html')
+    os.center()
+}
+
+// Iniciar a aplicação
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+// reduzir logs não críticos
+app.commandLine.appendSwitch('log-level', '3')
+
+// iniciar a conexão com o banco de dados (pedido direto do preload.js)
+ipcMain.on('db-connect', async (event) => {
+    let conectado = await conectar()
+    // se conectado for igual a true
+    if (conectado) {
+        // enviar uma mensagem para o renderizador trocar o ícone, criar um delay de 0.5s para sincronizar a nuvem
+        setTimeout(() => {
+            event.reply('db-status', "conectado")
+        }, 500) //500ms        
+    }
+})
+
+// IMPORTANTE ! Desconectar do banco de dados quando a aplicação for encerrada.
+app.on('before-quit', () => {
+    desconectar()
+})
+
+// template do menu
+const template = [
+    {
+        label: 'Cadastro',
+        submenu: [
+            {
+                label: 'Clientes',
+                click: () => clientWindow()
+            },
+            {
+                label: 'OS',
+                click: () => osWindow()
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label: 'Relatórios',
+        submenu: [
+            {
+                label: 'Clientes',
+                click: () => relatorioClientes()
+            },
+            {
+                label: 'OS abertas'
+            },
+            {
+                label: 'OS concluídas'
+            }
+        ]
+    },
+    {
+        label: 'Ferramentas',
+        submenu: [
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrão',
+                role: 'resetZoom'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Recarregar',
+                role: 'reload'
+            },
+            {
+                label: 'Ferramentas do desenvolvedor',
+                role: 'toggleDevTools'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Sobre',
+                click: () => aboutWindow()
+            }
+        ]
+    }
+]
+
+// recebimento dos pedidos do renderizador para abertura de janelas (botões) autorizado no preload.js
+ipcMain.on('client-window', () => {
+    clientWindow()
+})
+
+ipcMain.on('os-window', () => {
+    osWindow()
+})
+
+// ============================================================
+// == Clientes - CRUD Create
+// recebimento do objeto que contem os dados do cliente
+ipcMain.on('new-client', async (event, client) => {
+    // Importante! Teste de recebimento dos dados do cliente
+    console.log(client)
+    // Cadastrar a estrutura de dados no banco de dados MongoDB
+    try {
+        // criar uma nova de estrutura de dados usando a classe modelo. Atenção! Os atributos precisam ser idênticos ao modelo de dados Clientes.js e os valores são definidos pelo conteúdo do objeto cliente
+        const newClient = new clientModel({
+            nomeCliente: client.nameCli,
+            cpfCliente: client.cpfCli,
+            emailCliente: client.emailCli,
+            foneCliente: client.phoneCli,
+            cepCliente: client.cepCli,
+            logradouroCliente: client.addressCli,
+            numeroCliente: client.numberCli,
+            complementoCliente: client.complementCli,
+            bairroCliente: client.neighborhoodCli,
+            cidadeCliente: client.cityCli,
+            ufCliente: client.ufCli
+        })
+        // salvar os dados do cliente no banco de dados
+        await newClient.save()
+        // Mensagem de confirmação
+        dialog.showMessageBox({
+            //customização
+            type: 'info',
+            title: "Aviso",
+            message: "Cliente adicionado com sucesso",
+            buttons: ['OK']
+        }).then((result) => {
+            //ação ao pressionar o botão (result = 0)
+            if (result.response === 0) {
+                //enviar um pedido para o renderizador limpar os campos e resetar as configurações pré definidas (rótulo 'reset-form' do preload.js
+                event.reply('reset-form')
+            }
+        })
+    } catch (error) {
+        // se o código de erro for 11000 (cpf duplicado) enviar uma mensagem ao usuário
+        if (error.code === 11000) {
+            dialog.showMessageBox({
+                type: 'error',
+                title: "Atenção!",
+                message: "CPF já está cadastrado\nVerifique se digitou corretamente",
+                buttons: ['OK']
+            }).then((result) => {
+                if (result.response === 0) {
+                    // limpar a caixa de input do cpf, focar esta caixa e deixar a borda em vermelho
+                }
+            })
+        }
+        console.log(error)
+    }
+})
+
+// == Fim - Clientes - CRUD Create
+// ============================================================
+
+
+// ============================================================
+// == Relatório de clientes ===================================
+
+async function relatorioClientes() {
+    try {
+        // Passo 1: Consultar o banco de dados e obter a listagem de clientes cadastrados por ordem alfabética
+        const clientes = await clientModel.find().sort({ nomeCliente: 1 })
+        // teste de recebimento da listagem de clientes
+        //console.log(clientes)
+        // Passo 2:Formatação do documento pdf
+        // p - portrait | l - landscape | mm e a4 (folha)
+        const doc = new jsPDF('p', 'mm', 'a4')
+        // definir o tamanho da fonte (tamanho equivalente ao word)
+        doc.setFontSize(16)
+        // escrever um texto (título)
+        doc.text("Relatório de clientes", 14, 20)//x, y (mm)
+        // inserir a data atual no relatório
+        const dataAtual = new Date().toLocaleDateString('pt-BR')
+        doc.setFontSize(12)
+        doc.text(`Data: ${dataAtual}`, 165, 10)
+        // variável de apoio na formatação
+        let y = 45
+        doc.text("Nome", 14, y)
+        doc.text("Telefone", 80, y)
+        doc.text("E-mail", 130, y)
+        y += 5
+        // desenhar uma linha
+        doc.setLineWidth(0.5) // expessura da linha
+        doc.line(10, y, 200, y) // 10 (inicio) ---- 200 (fim)
+        // Definir o caminho do arquivo temporário e nome do arquivo
+        const tempDir = app.getPath('temp')
+        const filePath = path.join(tempDir, 'clientes.pdf')
+        // salvar temporariamente o arquivo
+        doc.save(filePath)
+        // abrir o arquivo no aplicativo padrão de leitura de pdf do computador do usuário
+        shell.openPath(filePath)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// == Fim - relatório de clientes =============================
+// ============================================================
